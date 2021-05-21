b1e7568e3d14b5a5a4f81aae0514bcca90a8a01a
Magniko <magniko1524@gmail.com>
2021-05-21 11:39:16 +0200

    added frontend branch

---
+++ main.py
@@ -0,0 +1,31 @@
+import logging
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/trafikaltgrunnkurs")
+def trafikaltgrunnkurs():
+	return render_template("trafikaltgrunnkurs/index.html")
+
+@app.route("/about")
+def about():
+	return render_template("about/index.html")
+
+@app.route("/contact")
+def contact():
+	return render_template("contact/index.html")
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
